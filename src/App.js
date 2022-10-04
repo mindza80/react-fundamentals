ee0529b9212ae80bdf85dbc2735bbcaae173f874
mindza80 <mindza@gmail.com>
2022-10-04 23:31:50 +0300

    I am new here!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+function FirstReactComponent(){
+  return(<p>I am new here!</p>)
+}
+
 function App() {
   return (
     <>
     <h1>Hello world</h1>
     <p>You look beautiful</p>
+    <FirstReactComponent />
     </>
   );
 }
