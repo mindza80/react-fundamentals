b9a6e681840f700e0238812ad51bb804e776dfbd
mindza80 <mindza@gmail.com>
2022-10-04 21:53:10 +0300

    looks beautiful

--- src/App.js
+++ src/App.js
@@ -3,22 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mindza's first React App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello world</h1>
+    <p>You look beautiful</p>
+    </>
   );
 }
 
