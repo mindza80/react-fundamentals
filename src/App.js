f0c40d383ae5e7cf2244bd71b92994d2b964bf7b
mindza80 <mindza@gmail.com>
2022-10-05 21:52:33 +0300

    I am Children

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+function ChildrenWithTitle({title, children}){
+  return (
+    <div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+}
+
 function FirstReactComponent(){
   return(<p>I am new here!</p>)
 }
 
 function App() {
   return (
-    <>
+    <div>
     <h1>Hello world</h1>
     <p>You look beautiful</p>
     <FirstReactComponent />
-    </>
+    <ChildrenWithTitle title="I am title">
+        I am children
+    </ChildrenWithTitle>
+    </div>
   );
 }
 
